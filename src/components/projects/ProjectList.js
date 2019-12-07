104ed84e7056ed6617227c992e6c9004605b5da5
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-12-07 12:55:41 +0400

    ProjectListing component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,32 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 export default function ProjectList() {
     return (
         <div className="project-list section">
-
-            <div className="card-content grey-text text-darken-3">
-                <div className="card z-depth-0 project-summary">
-                    <span className="card-title">Project Title 1</span>
-                    <p>Posted by NinjaRow</p>
-                    <p className="grey-text">3rd March, 2am</p>
-                </div>
-            </div>
-
-            <div className="card-content grey-text text-darken-3">
-                <div className="card z-depth-0 project-summary">
-                    <span className="card-title">Project Title 2</span>
-                    <p>Posted by NinjaCol</p>
-                    <p className="grey-text">3rd September, 8pm</p>
-                </div>
-            </div>
-
-            <div className="card-content grey-text text-darken-3">
-                <div className="card z-depth-0 project-summary">
-                    <span className="card-title">Project Title 3</span>
-                    <p>Posted by NinjaSec</p>
-                    <p className="grey-text">3rd December, 7am</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProjectSummary() {
+  return (
+    <div className="card-content grey-text text-darken-3">
+      <div className="card z-depth-0 project-summary">
+        <span className="card-title">Project Title 3</span>
+        <p>Posted by NinjaSec</p>
+        <p className="grey-text">3rd December, 7am</p>
+      </div>
+    </div>
+  );
+}
